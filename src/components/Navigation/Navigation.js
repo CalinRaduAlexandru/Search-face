7fa552c83ee71729607ada6aaa05cf55ee211802
Calin Radu <calinradualexandru@protonmail.com>
2021-01-29 07:08:46 +0200

    entire project with server and database on heroku

--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -1,20 +1,45 @@
-import React from 'react';
+import React from "react";
+import Logo from "../Logo/Logo";
+import "./Navigation.styles.css";
 
 const Navigation = ({ onRouteChange, isSignedIn }) => {
-    if (isSignedIn) {
-      return (
-        <nav style={{display: 'flex', justifyContent: 'flex-end'}}>
-          <p onClick={() => onRouteChange('signout')} className='f3 link dim black underline pa3 pointer'>Sign Out</p>
-        </nav>
-      );
-    } else {
-      return (
-        <nav style={{display: 'flex', justifyContent: 'flex-end'}}>
-          <p onClick={() => onRouteChange('signin')} className='f3 link dim black underline pa3 pointer'>Sign In</p>
-          <p onClick={() => onRouteChange('register')} className='f3 link dim black underline pa3 pointer'>Register</p>
-        </nav>
-      );
-    }
-}
+  if (isSignedIn) {
+    return (
+      <nav>
+        <div className="header">
+          <Logo className="logo" />
+          <div className="options">
+            <p
+              onClick={() => onRouteChange("signout")}
+              className="f3 link dim black underline pa3 pointer"
+            >
+              Sign Out
+            </p>
+          </div>
+        </div>
+      </nav>
+    );
+  } else {
+    return (
+      <nav className="header">
+        <Logo className="logo" />
+        <div className="options">
+          <p
+            onClick={() => onRouteChange("signin")}
+            className="f3 link dim black underline pa3 pointer"
+          >
+            Sign In
+          </p>
+          <p
+            onClick={() => onRouteChange("register")}
+            className="f3 link dim black underline pa3 pointer"
+          >
+            Register
+          </p>
+        </div>
+      </nav>
+    );
+  }
+};
 
 export default Navigation;
