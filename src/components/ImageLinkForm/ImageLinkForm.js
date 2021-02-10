8d7bf1895148fd01ffdd97475e9fd3f529077d0e
Calin Radu <calinradualexandru@protonmail.com>
2021-02-10 18:07:04 +0200

    cutting out the example text

--- src/components/ImageLinkForm/ImageLinkForm.js
+++ src/components/ImageLinkForm/ImageLinkForm.js
@@ -7,18 +7,6 @@ const ImageLinkForm = ({ onInputChange, onButtonSubmit, imageUrl }) => {
       <p className="f3">
         {"This Magic Search will detect faces in your pictures. Give it a try."}
       </p>
-      {imageUrl === "" ? (
-        <div className="exemple-box">
-          <h1>Exemple link:</h1>
-          <div className="text-box">
-            <div className="text-exemple">
-              https://i2-prod.mirror.co.uk/incoming/
-              article14334083.ece/ALTERNATES/s615/
-              3_Beautiful-girl-with-a-gentle-smile.jpg
-            </div>
-          </div>
-        </div>
-      ) : null}
       <div className="center">
         <div className="form center pa4 br3 shadow-5">
           <input
